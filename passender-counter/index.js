fe6e092a948698b7c2f53a69dea3861bb5bd0403
jacob-d242 <jacobmoracha@gmail.com>
2022-03-10 23:18:23 +0300

    day one creating variables

--- passender-counter/index.js
+++ passender-counter/index.js
@@ -1,1 +1,18 @@
-document.getElementById("count").innerText = 5
+////document.getElementById("count").innerText = 5
+
+//let count = 41
+//creating a variable to hold the variableage and its value
+
+//console.log(count)
+
+//let myAge = 22
+// let humanDogRatio = 2
+
+//let myDogAge = myAge * humanDogRatio
+
+//console.log(myDogAge)
+
+
+let count = 5
+
+console.log(count)
